70daa0b2baafad5bb1457f87fa586a413e8bc920
Allison Serio <aserio@mit.edu>
2020-12-07 12:17:49 -0800

    implemented best patch by l2 norm

--- TextureApp.py
+++ TextureApp.py
@@ -1,62 +1,64 @@
-# TODO: use matplotlib for demo??
 import numpy as np
 import random
 from itertools import product
 from PIL import Image
+from skimage import io, util
 
 def precompute_blocks(sample_image, block_size):
+    h, w, _ = sample_image.shape
     blocks = []
-    for i,j in product(range(sample_image.width-block_size), range(sample_image.height-block_size)):
-        block = sample_image.crop((i, j, i+block_size, j+block_size))
+    for i,j in product(range(h-block_size), range(w-block_size)):
+        block = sample_image[i:i+block_size, j:j+block_size]
         blocks.append(block)
     return blocks
 
-def synthesis(sample_image, output_width, output_height, block_size):
-    output_image = Image.new('RGB', (output_width, output_height))
+def synthesis(image_file, block_size, num_blocks):
+    sample_image = Image.open(image_file)
+    sample_image = util.img_as_float(sample_image)
     overlap = int(block_size/6)
+    output_width = num_blocks*block_size  - (num_blocks - 1) * overlap # second part is overlap area we need to subtract
+    output_height = num_blocks*block_size - (num_blocks - 1) * overlap
     blocks = precompute_blocks(sample_image, block_size)
 
-    for x in range(0, output_width-block_size, block_size-overlap):
-        for y in range(0, output_height-block_size, block_size-overlap):
+    res = np.zeros((output_height, output_width, sample_image.shape[2]))
+
+    for y in range(num_blocks): # rows
+        for x in range(num_blocks): # cols
+            i = y * (block_size - overlap)
+            j = x * (block_size - overlap)
+
             # first block, chose random loc in sample, and create patch
             if x == 0 and y == 0:
-                #sample_x, sample_y = np.random.randint(0,sample_image.width), np.random.randint(0, sample_image.height)
-                #first_patch = sample_image.crop((sample_x, sample_y, sample_x+block_size, sample_y+block_size))
-                first_patch = random.choice(blocks)
-                output_image.paste(first_patch)
+                patch = random.choice(blocks)
 
-            elif x == 0: # first column
-                # only overlap on top
-                prev_block = output_image.crop((0, y-block_size, block_size, y))
-                patch = findBestPatch(sample_image, blocks, prev_block, output_width, output_height, block_size, False, True)
-                output_image.paste(patch,(x,y-overlap))
-            # elif y = 0: # first row
-            #     # only overlap to left
-            #     pass
             else:
-                pass
+                patch = findBestPatch(sample_image, res, blocks, overlap, block_size, i, j)
+
+            res[i:i+block_size, j:j+block_size] = patch
+
+    output_image = Image.fromarray((res * 255).astype(np.uint8))
     return output_image
 
 
-def findBestPatch(sample_image, blocks, previous_block, output_width, output_height, block_size, horizontal, vertical):
+def findBestPatch(sample_image, res, blocks, overlap, block_size, i, j):
+    #h, w, _ = sample_image.shape
+    #errors = np.zeros((h - block_size, w - block_size))
     errors = []
     tolerance = 0.1
-    overlap = int(block_size/6)
     
     for block in blocks:
-        if horizontal:
-            pass
-        if vertical:
-            prev_overlap = previous_block.crop((0, block_size-overlap, block_size, block_size)) # bottom overlap
-            sample_overlap = block.crop((0,0, block_size, overlap)) # top overlap
-            ssd = 0
-            for x in range(overlap):
-                for y in range(overlap):
-                    ssd_r = ((sample_overlap.getpixel((x,y))[0] - prev_overlap.getpixel((x,y))[0])**2)**0.5
-                    ssd_g = ((sample_overlap.getpixel((x,y))[1] - prev_overlap.getpixel((x,y))[1])**2)**0.5
-                    ssd_b = ((sample_overlap.getpixel((x,y))[2] - prev_overlap.getpixel((x,y))[2])**2)**0.5
-                    ssd += (ssd_r + ssd_b + ssd_g)/3
-            errors.append(ssd)
+        error = 0
+        if j > 0: # left
+            left = block[:, :overlap] - res[i:i+block_size, j:j+overlap]
+            error += np.sum(left**2)
+        if i > 0: # right
+            up = block[:overlap, :] - res[i:i+overlap, j:j+block_size]
+            error += np.sum(up**2)
+        if j > 0 and i > 0: # need to get rid of corner once since calc twice
+            corner = block[:overlap, :overlap] - res[i:i+overlap, j:j+overlap]
+            error -= np.sum(corner**2)
+        #errors[i, j] = error
+        errors.append(error)
 
     min_error = min(errors)
     candidates = list(filter(lambda x: x <= min_error+tolerance*min_error, errors))
--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import sys
 from PIL import Image
-
+import argparse
+import os
 import TextureApp
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--image_path", required=True, type=str, help="path of image you want to quilt")
+parser.add_argument("-b", "--block_size", type=int, default=50, help="block size in pixels")
+parser.add_argument("-n", "--num_block", type=int, default=6, help="number of blocks you want")
+parser.add_argument("-o","--output_file", required=True, type=str, help="Need output file path")
+#parser.add_argument("-w","--output_width",required=True,type=int,help="Desired width of image")
+#parser.add_argument("-h","--output_height",required=True,type=int,help="Desired height of image")
+args = parser.parse_args()
 
 '''
 Main driver, takes in sample image file, output image file, and block size of patch
 '''
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    if (len(args) < 5):
-        sys.exit("Not enough arguments \n SAMPLE_IMAGE OUTPUT_TEXTURE WIDTH HEIGHT BLOCK_SIZE needed")
-
-    sample_image = Image.open(args[0])
-    output_texture = args[1]
-    output_width = int(args[2])
-    output_height = int(args[3])
-    block_size = int(args[4])
+    image_file = args.image_path
+    block_size = args.block_size
+    num_block = args.num_block
+    output_texture = args.output_file
+    #output_texture = args[1]
+    #output_width = int(args[2])
+    #output_height = int(args[3])
+    
 
     if (block_size <= 0):
         sys.exit("Invalid block size")
 
-    texture = TextureApp.synthesis(sample_image, output_width, output_height, block_size) # texture an image
+    texture = TextureApp.synthesis(image_file, block_size, num_block) # texture an image
     texture.save(output_texture)
     
 
